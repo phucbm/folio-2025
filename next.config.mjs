32e7603ac14169cda5e5b8604037cf81972800f3
phucbm <hiphucbui@gmail.com>
2025-10-28 20:00:21 +0700

    feat(analytics): add posthog

--- next.config.mjs
+++ next.config.mjs
@@ -8,4 +8,18 @@ const withNextra = nextra({
 // You can include other Next.js configuration options here, in addition to Nextra settings:
 export default withNextra({
   // ... Other Next.js config options
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://us-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://us.i.posthog.com/:path*',
+      },
+    ]
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 })
