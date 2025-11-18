bd747c4716a061b99a105da1112a50304f94618e
phucbm <hiphucbui@gmail.com>
2025-11-18 18:04:59 +0700

    feat: install jsdelivr

---
+++ src/lib/formatOrdinal.ts
@@ -0,0 +1,17 @@
+export function formatOrdinal(num: number): string {
+    // Format number with commas
+    const formattedNum = num.toLocaleString('en-US')
+
+    const j = num % 10
+    const k = num % 100
+    if (j === 1 && k !== 11) {
+        return formattedNum + "st"
+    }
+    if (j === 2 && k !== 12) {
+        return formattedNum + "nd"
+    }
+    if (j === 3 && k !== 13) {
+        return formattedNum + "rd"
+    }
+    return formattedNum + "th"
+}
