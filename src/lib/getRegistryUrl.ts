f2400643afe4621d9363189c10d7e8183d5e1806
phucbm <hiphucbui@gmail.com>
2025-12-30 16:28:55 +0700

    refactor: change registry namespace

--- src/lib/getRegistryUrl.ts
+++ src/lib/getRegistryUrl.ts
@@ -2,5 +2,6 @@ export function getRegistryUrl({name, fileNamePostfix = ''}: { name: string, fil
     const folder = process.env.NEXT_PUBLIC_REGISTRY_FOLDER || "r";
 
     // point to example of the registry so v0 can deploy with full demo
-    return `${process.env.NEXT_PUBLIC_SITE_URL}/${folder}/${name}${fileNamePostfix}.json`;
+    return `@phucbm/${name}${fileNamePostfix}`;
+    // return `${process.env.NEXT_PUBLIC_SITE_URL}/${folder}/${name}${fileNamePostfix}.json`;
 }
